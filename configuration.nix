2d61765d4f4a06e37b7169214a787e630914d0a1
Philpax <me@philpax.me>
2024-04-06 21:48:43 +0200

    Remove comments, fixed IP, reorganise, lunarvim

--- configuration.nix
+++ configuration.nix
@@ -1,41 +1,37 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       ./no-rgb-service.nix
     ];
 
-  # Bootloader.
+  system.stateVersion = "23.11";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "redline"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 7860 8000 ];
+  networking = {
+    hostName = "redline";
+    networkmanager.enable = true;
+    firewall.allowedTCPPorts = [ 22 7860 8000 ];
+    interfaces.enp67s0f1 = {
+      useDHCP = false;
+      ipv4.addresses = [
+	{
+	  address = "192.168.50.201";
+	  prefixLength = 24;
+	}
+      ];
+    };
+    defaultGateway = "192.168.50.1";
+    nameservers = ["1.1.1.1" "1.0.0.1"];
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_GB.UTF-8";
     LC_IDENTIFICATION = "en_GB.UTF-8";
@@ -48,14 +44,6 @@
     LC_TIME = "en_GB.UTF-8";
   };
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "au";
-    xkbVariant = "";
-    videoDrivers = ["nvidia"];
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.philpax = {
     isNormalUser = true;
     description = "philpax";
@@ -66,11 +54,6 @@
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
     linuxPackages.nvidia_x11
@@ -83,41 +66,19 @@
     openssl.dev
     pkg-config
     screen
+    lunarvim
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings.PasswordAuthentication = false;
-  services.hardware.openrgb.enable = true; 
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  services.openssh.enable = true;
+  services.openssh.settings.PasswordAuthentication = false;
+  services.hardware.openrgb.enable = true;
+  services.xserver = {
+    layout = "au";
+    xkbVariant = "";
+    videoDrivers = ["nvidia"];
+  };
 }
