252075dd86c59932d0ff1018d9cf8c05ec190235
Philpax <me@philpax.me>
2024-04-10 22:45:12 +0200

    chore: switch to ondemand governor

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  powerManagement.cpuFreqGovernor = "ondemand";
+
   networking = {
     hostName = "redline";
     networkmanager.enable = true;
