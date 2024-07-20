466e910266779da8d0fe43105763cfed97587e8b
Philpax <me@philpax.me>
2024-07-20 22:59:34 +0200

    feat: libvirt???

--- configuration.nix
+++ configuration.nix
@@ -16,6 +16,8 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
   fileSystems = {
     "/mnt/ssd2" = {
@@ -56,6 +58,21 @@ in
 
   virtualisation.docker.enable = true;
   virtualisation.docker.enableNvidia = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 
   networking = {
     hostName = "redline";
@@ -66,6 +83,7 @@ in
       8000 # python -m http.server
       8384 # syncthing GUI
       22000 # syncthing traffic
+      5900 5901 5902 # spice/vnc
     ];
     firewall.allowedUDPPorts =  [
       22000 # syncthing traffic
@@ -100,7 +118,7 @@ in
   users.users.philpax = {
     isNormalUser = true;
     description = "philpax";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "libvirtd" "kvm" ];
     packages = with pkgs; [];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDwvEVMGi643L4ufnpEPLHgSIBs2pN1BMG7Z2SGlKPf8N/SjpjKmyUE9NJw1ACb/wQ7D83c+r1QSbW4PUgq1uIuLdOteNj6+QeTiXKW3rmDIQQy0TzV0v/KP5YxK2EXCtr1Bv7Ca/WVLcUzIkvp8xzvXXgB58FbrveRzBYMIiieQYXMvd70HkliccrczyIc0x2mE8KqXy3/TFnZHAw96AenIPcifLenQgSIDsds1JTJoyNWHNa1ac/UKrlzKqNzX2apdL8vX2W+FeR/IZ+Mi86coGR42LJvktYWexqs+876UhMvha4L5toKkqVMf/JH7E3YUt/TbXBykR2rRyxrzYpFUWrk/wL+si30YWK+6a4jD8RDtGzKy+sWM7xitJPaamE9k3bSmexBu3wSc8UCvWyOmHs/YAoFeJIKUET7b3sRKMZbt2tmR//JJdL+PdUsxX7T1JJt/z0wbFK+ENYJVPYUE/B/o8isBkpBdy0pJs7SVjT52wM0JrMqaqAN8HrfUzKt9N8HTaztCGjv86y/avH9it1gERDMTef6HaXROiQngdrChOjQ0nysfIxnsh48usD+p8VbXb54VZM0wRmPUgoUKZbro7AsHvtCNfNI1oBHYFTTIZsGHML5Ho8OlZ8XVTgaIufZc+ZkYN2lRXZPwhQwiIg3Kz0kMP5Uo4onMJOIJw== me@philpax.me"
@@ -126,6 +144,10 @@ in
     jq
     rye
     ntfs3g
+    virt-manager
+    qemu
+    OVMF
+    virt-viewer
   ];
   programs.neovim = {
     enable = true;
