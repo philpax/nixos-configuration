8a454852e5acdaf822b6f376288aac5b77e76482
Philpax <me@philpax.me>
2025-06-13 03:07:10 +0200

    feat: tailscale routing

--- nixos/services/misc.nix
+++ nixos/services/misc.nix
@@ -4,6 +4,7 @@
   services.hardware.openrgb.enable = true;
   services.resolved.enable = true;
   services.tailscale.enable = true;
+  services.tailscale.useRoutingFeatures = "both";
   services.udisks2.enable = true;
   services.devmon.enable = true;
 }
