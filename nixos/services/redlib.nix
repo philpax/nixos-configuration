a0d17c9f56b394a690170c6fdba7317f9512ae41
Philpax <me@philpax.me>
2025-07-30 01:10:34 +0200

    feat(services): add defaults to redlib

--- nixos/services/redlib.nix
+++ nixos/services/redlib.nix
@@ -6,5 +6,26 @@
     openFirewall = true;
     package = unstable.redlib;
     port = 10000;
+    settings = {
+      REDLIB_SFW_ONLY = "off";
+      REDLIB_BANNER = "philpax's personal redlib. you probably shouldn't have access to this, unless you're me.";
+      REDLIB_ROBOTS_DISABLE_INDEXING = "on";
+      REDLIB_DEFAULT_FRONT_PAGE = "default";
+      REDLIB_DEFAULT_LAYOUT = "compact";
+      REDLIB_DEFAULT_WIDE = "off";
+      REDLIB_DEFAULT_BLUR_SPOILER = "on";
+      REDLIB_DEFAULT_BLUR_NSFW = "on";
+      REDLIB_DEFAULT_SUBSCRIPTIONS = builtins.concatStringsSep "+" [
+        "datahoarder"
+        "fujifilm"
+        "LocalLLaMA"
+        "hardware"
+        "rust"
+        "selfhosted"
+        "StableDiffusion"
+        "steinsgate"
+      ];
+      REDLIB_DEFAULT_REMOVE_DEFAULT_FEEDS = "on";
+    };
   };
 }
