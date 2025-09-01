efb837caaec2a6bd06e9db9c686988ab6137e4bb
Philpax <me@philpax.me>
2025-09-01 03:29:20 +0200

    feat: sync notes to phone

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -11,13 +11,13 @@
     guiAddress = "127.0.0.1:8384";
     settings = {
       devices = {
-        "work-mbp" = { id = "755IIFA-4U6ZX4Z-MYVIMZT-6BR5MDT-UDGV42J-CDXBRC7-RVC26M2-XAEO3AB"; };
+        "iphone" = { id = "CTLWMSO-UZTMF6D-DKMOSXI-4DST6YP-W2GN3YW-Y5AV4UD-LUXBIBV-WBJXXQ6"; };
         "the-wind-rises" = { id = "NLD2NYH-SAYR2TR-GSRXTMD-EWIQCYN-RNI2UDA-52QQEZX-FVVC3NC-YSPWYAY"; };
       };
       folders = {
-        "Notes" = {
-          path = "/mnt/ssd2/notes";
-          devices = [ "work-mbp" "the-wind-rises" ];
+        "Main" = {
+          path = "/mnt/ssd2/notes/Main";
+          devices = [ "iphone" "the-wind-rises" ];
         };
       };
     };
