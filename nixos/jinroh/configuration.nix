3348ffdf813cec6f24349391701b6bb82477b825
Philpax <me@philpax.me>
2025-09-02 21:09:03 +0200

    feat(jinroh): remove X11 tripe

--- nixos/jinroh/configuration.nix
+++ nixos/jinroh/configuration.nix
@@ -17,19 +17,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # services.xserver.enable = true;
-
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb = {
-  #   layout = "au";
-  #   variant = "";
-  # };
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
