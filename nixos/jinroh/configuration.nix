f80e34831f2b7c995321cc172d90d6a291a25ff5
Philpax <me@philpax.me>
2025-09-21 23:10:29 +0200

    feat(jinroh): grub -> systemd-boot

--- nixos/jinroh/configuration.nix
+++ nixos/jinroh/configuration.nix
@@ -8,9 +8,8 @@
 
   system.stateVersion = "24.11";
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "jinroh";
 
