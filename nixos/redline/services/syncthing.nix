8f0457dcd9472c38a33ac2cce17f4802ae0d64db
Philpax <me@philpax.me>
2025-12-05 19:02:10 +0100

    feat(redline): add paprika to syncthing

--- nixos/redline/services/syncthing.nix
+++ nixos/redline/services/syncthing.nix
@@ -12,11 +12,12 @@
       devices = {
         "iphone" = { id = "CTLWMSO-UZTMF6D-DKMOSXI-4DST6YP-W2GN3YW-Y5AV4UD-LUXBIBV-WBJXXQ6"; };
         "the-wind-rises" = { id = "NLD2NYH-SAYR2TR-GSRXTMD-EWIQCYN-RNI2UDA-52QQEZX-FVVC3NC-YSPWYAY"; };
+        "paprika" = { id = "6AJNFQE-DCDVHBD-UVPZINA-5HGY655-VGVCG2H-X7D7EL6-IEWWMIZ-F2LJSAQ"; };
       };
       folders = {
         "Main" = {
           path = "/mnt/ssd0/notes/Main";
-          devices = [ "iphone" "the-wind-rises" ];
+          devices = [ "iphone" "the-wind-rises" "paprika" ];
         };
       };
       options = {
