47646edf8df335fd4bcaff2033e17fc612384cca
Philpax <me@philpax.me>
2025-12-24 02:28:12 +1100

    feat(dotfiles): add nix shell indicator to prompt

---
+++ dotfiles/common-all/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,38 @@
+# Based on the default fish prompt, with nix-shell indicator added
+# Original author: Lily Ballard
+
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+
+    # Nix shell indicator
+    set -l nix_shell_info
+    if test -n "$IN_NIX_SHELL"
+        set nix_shell_info (set_color brmagenta)"(nix) "$normal
+    end
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix '>'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # Write pipestatus
+    # If the status was carried over (if no command is issued or if `set` leaves the status untouched), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color $bold_flag $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+
+    echo -n -s $nix_shell_info (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+end
