16640b1982b7922520edf89570938e623ae5bf77
Philpax <me@philpax.me>
2026-05-15 20:32:41 +0200

    feat: update nixpkgs-xr version

--- nixos/mindgame/nixpkgs-xr.nix
+++ nixos/mindgame/nixpkgs-xr.nix
@@ -1,7 +1,7 @@
 { ... }:
 let
   nixpkgs-xr = import
-    (builtins.fetchTarball "https://github.com/nix-community/nixpkgs-xr/archive/665e1550f5411756b3cb678dbdca878f164814ea.tar.gz");
+    (builtins.fetchTarball "https://github.com/nix-community/nixpkgs-xr/archive/c6af4789d7801fa243205a8ce9b8e3feffbf42b4.tar.gz");
 in
 {
   imports = [ nixpkgs-xr.nixosModules.nixpkgs-xr ];
